d5e61263a1e033bee66c65bc91f81d8b9a28fc0f
Guilherme Oliveira <oguilhermeDeveloper@gmail.com>
2025-05-19 03:35:09 -0300

    Autenticação funcionando corretamente
    criação de conta, login, logout e feedback visual funcionando corretamente. Alguns ajustes visuais podem ser feitos. Recomendo uma pagina de adm para remover usuarios

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,3 +129,10 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
